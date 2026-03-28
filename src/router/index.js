93e1df78606e7866bb8a838589e4eb081445b3c2
ailosasa <644792544@qq.com>
2026-03-28 09:04:05 +0800

    关闭党建BC票

--- src/router/index.js
+++ src/router/index.js
@@ -25,7 +25,7 @@ import Dept_sc from "../views/Dept_sc.vue";
 import Dept_sj from "../views/Dept_sj.vue";
 import Dept_zh from "../views/Dept_zh.vue";
 import Dept_zl from "../views/Dept_zl.vue";
-import Dept_yja from "../views/yeji_A.vue";
+//import Dept_yja from "../views/yeji_A.vue";
 
 const routes = [
     { path: '/', redirect: '/a' }, // 默认打开A类票
@@ -52,7 +52,7 @@ const routes = [
     { path: '/dept_sj', component: Dept_sj },
     { path: '/dept_zh', component: Dept_zh },
     { path: '/dept_zl', component: Dept_zl },
-    { path: '/dept_yja', component: Dept_yja }
+    //{ path: '/dept_yja', component: Dept_yja }
 ]
 
 const router = createRouter({
